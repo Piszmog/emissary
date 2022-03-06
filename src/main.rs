102ff4bd1b65ff9b1824820ab612295bc7d095f2
Piszmog <piszmogcode@gmail.com>
2022-03-05 20:00:03 -0700

    crate changes and add the logging middleware

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,43 @@
 use actix_web::{App, HttpServer, web};
 use awc::Client;
+use clap::Parser;
+use tracing_subscriber;
 
-use cli::cli::parse_args;
-use config::config::read_toml_file;
+use config::read_toml_file;
 use handlers::{info, proxy};
 use handlers::info::Info;
 use middleware::logging;
-use utils::net::to_url;
 
 mod handlers;
 mod middleware;
 mod config;
-mod cli;
 mod utils;
 
+/// The main entry point of the application.
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let args = parse_args();
-    let config_file = read_toml_file(args.config_file);
+    let args = Args::parse();
+    let config_file = read_toml_file(args.config_file).unwrap();
 
-    let forward_url = to_url(config_file.proxy.address, config_file.proxy.port);
+    tracing_subscriber::fmt()
+        .with_level(false)
+        .with_file(false)
+        .without_time()
+        .with_target(false)
+        .init();
+
+    let json = utils::convert(config_file.logging.json.format);
 
     HttpServer::new(move || {
         App::new()
             // data to share across requests/handlers
             .app_data(web::Data::new(Client::default()))
-            .app_data(web::Data::new(forward_url.clone()))
-            .app_data(web::Data::new(Info::new().clone()))
+            .app_data(web::Data::new(utils::to_url(config_file.proxy.address.clone(), config_file.proxy.port)))
+            .app_data(web::Data::new(Info::new()))
             // middleware
-            .wrap(logging::SayHi)
+            .wrap(logging::Logging { json: json.clone() })
             // handlers
-            .default_service(web::to(proxy::forward))
+            .default_service(web::to(proxy::proxy))
             .service(
                 web::scope("/emissary")
                     .route("/info", web::get().to(info::info))
@@ -39,4 +46,13 @@ async fn main() -> std::io::Result<()> {
         .bind((config_file.http.address, config_file.http.port))?
         .run()
         .await
+}
+
+/// The struct that holds the CLI arguments.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    /// The path to the configuration file.
+    #[clap(short, long, default_value_t = String::from("./config.toml"))]
+    pub config_file: String,
 }
