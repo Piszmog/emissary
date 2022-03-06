272cdfb8e8c8e999d4a343d736b44260d6e547b5
Piszmog <piszmogcode@gmail.com>
2022-03-05 20:09:59 -0700

    add docs

--- src/middleware/logging.rs
+++ src/middleware/logging.rs
@@ -15,6 +15,7 @@ use serde_json::{Number, Value as Json};
 use tracing::info;
 use uuid::Uuid;
 
+/// The Logging middleware.
 pub struct Logging {
     pub json: Json,
 }
@@ -36,6 +37,7 @@ impl<S: 'static, B> Transform<S, ServiceRequest> for Logging
     }
 }
 
+/// The actual logging middleware. Handles the request/response.
 pub struct LoggingMiddleware<S> {
     service: Rc<S>,
     json: Json,
@@ -64,31 +66,36 @@ impl<S, B> Service<ServiceRequest> for LoggingMiddleware<S>
             let res = svc.call(req).await?;
             let end = Instant::now();
 
+            // we have everything we need to log, replace the JSON with the actual values
             populate(&mut json, &Context {
                 start,
                 end,
                 req_head: head,
-                status_code: res.status().as_u16(),
                 res_head: res.response().head().clone(),
             });
 
             info!("{}", json);
-
             Ok(res)
         })
     }
 }
 
-
+/// The context of the request/response.
 struct Context {
-    pub start: Instant,
-    pub end: Instant,
-    pub req_head: RequestHead,
-    pub status_code: u16,
-    pub res_head: ResponseHead,
+    /// The start time of the request.
+    start: Instant,
+    /// The end time of the request.
+    end: Instant,
+    /// The request head.
+    req_head: RequestHead,
+    /// The response head.
+    res_head: ResponseHead,
 }
 
+/// Populate the JSON with the context information. Some of the fields may have values that need to
+/// be calculated at runtime.
 fn populate(value: &mut Json, context: &Context) {
+    // create the regexes once
     lazy_static! {
         static ref RE_REQ_HEADER: Regex = Regex::new(r"%\{REQUEST_HEADER\((.*)\)}" ).unwrap();
         static ref RE_RES_HEADER: Regex = Regex::new(r"%\{RESPONSE_HEADER\((.*)\)}" ).unwrap();
@@ -113,8 +120,10 @@ fn populate(value: &mut Json, context: &Context) {
                 "%{DURATION(S)}" => *s = duration(context.start, context.end, "s").unwrap(),
                 "%{METHOD}" => *s = context.req_head.method.to_string(),
                 "%{URI}" => *s = context.req_head.uri.to_string(),
-                "%{STATUS_CODE}" => *value = Json::Number(Number::from(context.status_code)),
+                "%{STATUS_CODE}" => *value = Json::Number(Number::from(context.res_head.status.as_u16())),
                 _ => {
+                    // To handle HTTP headers, we need to use the regexes. Since the key cannot be
+                    // known ahead of time.
                     match RE_REQ_HEADER.captures(s) {
                         Some(cap) => {
                             *s = header(context.req_head.headers(), &cap[1]);
@@ -134,21 +143,29 @@ fn populate(value: &mut Json, context: &Context) {
     }
 }
 
+/// Generate a UUID.
 fn uuid() -> String {
     Uuid::new_v4().to_string()
 }
 
+/// Returns the duration between two instants. The unit is specified by the parameter `duration_type`.
 fn duration(start: Instant, end: Instant, duration_type: &str) -> Result<String, String> {
     match duration_type {
+        // Nanoseconds
         "ns" => Ok(end.duration_since(start).as_nanos().to_string()),
+        // Milliseconds
         "ms" => Ok(end.duration_since(start).as_millis().to_string()),
+        // Seconds
         "s" => Ok(end.duration_since(start).as_secs().to_string()),
+        // Not supported
         _ => Err(format!("{} is not a valid duration type", duration_type)),
     }
 }
 
+/// Returns the value of the header specified by the parameter `key`.
 fn header(headers: &HeaderMap, key: &str) -> String {
     headers.get(key)
         .map(|header| header.to_str().unwrap().to_string())
+        // If the header is not found, return an empty string
         .unwrap_or_else(|| "".to_string())
 }
