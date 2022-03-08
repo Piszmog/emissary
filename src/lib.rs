5ed2fb6d4fcf8a9219730101636c74b39e35780f
Piszmog <piszmogcode@gmail.com>
2022-03-08 14:09:00 -0700

    update the Extension interface to include the event function, and rename the modify req/res functions. Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,25 @@
 use actix_web::dev::{ServiceRequest, ServiceResponse};
 
 pub trait Extension {
+    /// Determines the order in which extensions are applied. Lower the number, the earlier the
+    /// extension is applied.
     fn order(&self) -> usize {
         usize::MAX
     }
+
+    /// Determines if the request should continue to be processed.
+    ///
+    /// Used in authorization/authentication scenarios.
     fn guard(&self, req: &ServiceRequest) -> bool {
         true
     }
-    fn before_request(&self, req: &mut ServiceRequest) {}
-    fn after_response(&self, res: &mut ServiceResponse) {}
+
+    /// Invoked when the request is processed. Can be used for logging, etc.
+    fn event(&self, req: &ServiceRequest, res: &ServiceResponse) {}
+
+    /// Modifies the request. Can be used to add/remove HTTP Headers, modify the request body, etc.
+    fn modify_request(&self, req: &mut ServiceRequest) {}
+
+    /// Modifies the response. Can be used to add/remove HTTP Headers, modify the response body, etc.
+    fn modify_response(&self, res: &mut ServiceResponse) {}
 }
